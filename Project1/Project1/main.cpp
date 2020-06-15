2a1c14248926d783de510ad8afb48b802be10405
bestway27@gmail.com <bestway27@gmail.com>
2020-06-16 08:47:23 +0900

    prob-14502  all solution

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,60 +1,76 @@
 #include <cstdio>
-#define FOR(a,b) for(int a = 0; a < b; a++)
-int n, m;
+#define FOR(a,b,c) for(int a = b; a < c; a++)
+#define FORFOR(i,j,n,m) FOR(i,0,n)FOR(j,0,m)
+#define MAP(i) map.v[i / m][i - (i / m) * m]
+
 struct Map {
 	int v[8][8];
 };
+int n, m, cnt = 0;
 Map map;
-void make_wall() {
-	FOR(i, n * m) {
-		FOR(j, n * m) {
-
-			FOR(k, n * m) {
-				map[j / m][j - (j / m) * m] = 1;
-				map[k / m][k - (k / m) * m] = 1;
-			}
-		}
-	}
-}
-void spread_virus(Map map, int i, int j) {
-	if (i < 0 || i >= n || j < 0 || j >= m || !map[i][j])
-		return;
-	map[i][j] = 2;
-	spread_virus(i - 1, j);
-	spread_virus(i + 1, j);
-	spread_virus(i, j - 1);
-	spread_virus(i, j + 1);
-}
-int count_safe_zone(Map map) {
+int count_safe_zone(Map& map) {
 	int c = 0;
-	FOR(i, n)
-		FOR(j, m)
-		if (!map[i][j])
+	FORFOR(i,j,n,m)
+		if (!map.v[i][j])
 			c++;
 	return c;
 }
+void dfs(Map& map, int i, int j) {
+	if (i < 0 || i >= n || j < 0 || j >= m
+		|| (map.v[i][j] != 2 && map.v[i][j]))
+		return;
+	map.v[i][j] = 3;
+	dfs(map, i - 1, j);
+	dfs(map, i + 1, j);
+	dfs(map, i, j - 1);
+	dfs(map, i, j + 1);
+}
+void spread_virus(Map map) {
+	FORFOR(i, j, n, m)
+		if (map.v[i][j] == 2)
+			dfs(map, i, j);
+	int max = count_safe_zone(map);
+	if (max > cnt)
+		cnt = max;
+}
+//void make_wall_iter() {
+//	FOR(i, 0, n * m) {
+//		if (MAP(i))
+//			continue;
+//		MAP(i) = 1;
+//		FOR(j, i, n * m) {
+//			if (MAP(j))
+//				continue;
+//			MAP(j) = 1;
+//			FOR(k, j, n * m) {
+//				if (MAP(k))
+//					continue;
+//				MAP(k) = 1;
+//				spread_virus(map);
+//				MAP(k) = 0;
+//			}
+//			MAP(j) = 0;
+//		}
+//		MAP(i) = 0;
+//	}
+//}
+void make_wall(int wall, int i) {
+	if (!wall) {
+		spread_virus(map);
+		return;
+	}
+	FOR(j, i, n*m) {
+		if (MAP(j))
+			continue;
+		MAP(j) = 1;
+		make_wall(wall - 1, j);
+		MAP(j) = 0;
+	}
+}
 int main() {
 	scanf("%d%d", &n, &m);
-	FOR(i, n)
-		FOR(j, m)
+	FORFOR(i, j, n, m)
 		scanf("%d", &map.v[i][j]);
+	make_wall(3, 0);
+	printf("%d", cnt);
 }
-
-
-
-
-
-//void a() {
-//	for (int i = 0; i < 3; i++)
-//		for (int j = 0; j < 3; j++)
-//			for (int k = 0; k < 3; k++)
-//				printf("%d %d %d\n", i, j, k);
-//}
-//void b(int i, int j, int k) {
-//	if (k > 2) { k = 0, j++; }
-//	if (j > 2) { j = 0, i++; }
-//	if (i > 2) return;
-//	printf("%d %d %d\n", i, j, k);
-//	k++;
-//	b(i, j, k);
-//}
