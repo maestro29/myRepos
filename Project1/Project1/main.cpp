caf72ec30b76edb389af5a4452c4cbc25e685b82
bestway27@gmail.com <bestway27@gmail.com>
2020-06-16 09:31:27 +0900

    prob-14502 optimal solution

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,17 +1,16 @@
 #include <cstdio>
 #define FOR(a,b,c) for(int a = b; a < c; a++)
-#define FORFOR(i,j,n,m) FOR(i,0,n)FOR(j,0,m)
 #define MAP(i) map.v[i / m][i - (i / m) * m]
 
 struct Map {
 	int v[8][8];
 };
-int n, m, cnt = 0;
+int n, m, max = 0;
 Map map;
 int count_safe_zone(Map& map) {
 	int c = 0;
-	FORFOR(i,j,n,m)
-		if (!map.v[i][j])
+	FOR(i, 0, n*m)
+		if (!MAP(i))
 			c++;
 	return c;
 }
@@ -26,34 +25,14 @@ void dfs(Map& map, int i, int j) {
 	dfs(map, i, j + 1);
 }
 void spread_virus(Map map) {
-	FORFOR(i, j, n, m)
+	FOR(i, 0, n)
+	FOR(j, 0, m)
 		if (map.v[i][j] == 2)
 			dfs(map, i, j);
-	int max = count_safe_zone(map);
-	if (max > cnt)
-		cnt = max;
+	int cnt = count_safe_zone(map);
+	if (cnt > max)
+		max = cnt;
 }
-//void make_wall_iter() {
-//	FOR(i, 0, n * m) {
-//		if (MAP(i))
-//			continue;
-//		MAP(i) = 1;
-//		FOR(j, i, n * m) {
-//			if (MAP(j))
-//				continue;
-//			MAP(j) = 1;
-//			FOR(k, j, n * m) {
-//				if (MAP(k))
-//					continue;
-//				MAP(k) = 1;
-//				spread_virus(map);
-//				MAP(k) = 0;
-//			}
-//			MAP(j) = 0;
-//		}
-//		MAP(i) = 0;
-//	}
-//}
 void make_wall(int wall, int i) {
 	if (!wall) {
 		spread_virus(map);
@@ -69,8 +48,8 @@ void make_wall(int wall, int i) {
 }
 int main() {
 	scanf("%d%d", &n, &m);
-	FORFOR(i, j, n, m)
-		scanf("%d", &map.v[i][j]);
+	FOR(i, 0, n*m)
+		scanf("%d", &MAP(i));
 	make_wall(3, 0);
-	printf("%d", cnt);
+	printf("%d", max);
 }
