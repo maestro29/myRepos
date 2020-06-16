49c45a793226aad26ba433d55faacc241df3735f
bestway27@gmail.com <bestway27@gmail.com>
2020-06-16 12:38:43 +0900

    last

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,55 +1,26 @@
 #include <cstdio>
-#define FOR(a,b,c) for(int a = b; a < c; a++)
-#define MAP(i) map.v[i / m][i - (i / m) * m]
 
-struct Map {
-	int v[8][8];
-};
-int n, m, max = 0;
-Map map;
-int count_safe_zone(Map& map) {
-	int c = 0;
-	FOR(i, 0, n*m)
-		if (!MAP(i))
-			c++;
-	return c;
+int k, n[13], a[6];
+void print() {
+	for (int i = 0; i < 6; i++)
+		printf("%d ", a[i]);
+	printf("\n");
 }
-void dfs(Map& map, int i, int j) {
-	if (i < 0 || i >= n || j < 0 || j >= m
-		|| (map.v[i][j] != 2 && map.v[i][j]))
-		return;
-	map.v[i][j] = 3;
-	dfs(map, i - 1, j);
-	dfs(map, i + 1, j);
-	dfs(map, i, j - 1);
-	dfs(map, i, j + 1);
-}
-void spread_virus(Map map) {
-	FOR(i, 0, n)
-	FOR(j, 0, m)
-		if (map.v[i][j] == 2)
-			dfs(map, i, j);
-	int cnt = count_safe_zone(map);
-	if (cnt > max)
-		max = cnt;
-}
-void make_wall(int wall, int i) {
-	if (!wall) {
-		spread_virus(map);
+void f(int d, int i) {
+	if (d > 5) {
+		print();
 		return;
 	}
-	FOR(j, i, n*m) {
-		if (MAP(j))
-			continue;
-		MAP(j) = 1;
-		make_wall(wall - 1, j);
-		MAP(j) = 0;
+	for (int j = i; j < k; j++) {
+		a[d] = n[j];
+		f(d + 1, j + 1);
 	}
 }
 int main() {
-	scanf("%d%d", &n, &m);
-	FOR(i, 0, n*m)
-		scanf("%d", &MAP(i));
-	make_wall(3, 0);
-	printf("%d", max);
+	for (; scanf("%d", &k) && k;) {
+		for (int i = 0; i < k; i++)
+			scanf("%d", &n[i]);
+		f(0, 0);
+		printf("\n");
+	}
 }
