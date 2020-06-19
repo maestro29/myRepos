cf5133c18a73ef5bb9f3fe064bc1128e797f4c42
bestway27@gmail.com <bestway27@gmail.com>
2020-06-19 17:47:36 +0900

    LAST

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 using namespace std;
-int r, c, ans = 0;
-char s[20][20];
-void dfs(int i, int j) {
-	if (i < 0 || i >= r || j < 0 || j >= c || !s[i][j])
+int r, c, ans, v[26];
+char s[21][21];
+void dfs(int cnt, int i, int j) {
+	if (i < 0 || i >= r || j < 0 || j >= c || v[s[i][j] - 'A'])
 		return;
-	ans++;
-	s[i][j] = 0;
-	dfs(i - 1, j);
-	dfs(i + 1, j);
-	dfs(i, j - 1);
-	dfs(i, j + 1);
+	v[s[i][j] - 'A'] = 1;
+	if (++cnt > ans)
+		ans = cnt;
+	dfs(cnt, i - 1, j);
+	dfs(cnt, i + 1, j);
+	dfs(cnt, i, j - 1);
+	dfs(cnt, i, j + 1);
+	//v[s[i][j]] = 0;
 }
 int main() {
 	ios::sync_with_stdio(0);
 	scanf("%d%d", &r, &c);
-	for (int i = 0; i < r; i++) {
+	for (int i = 0; i < r; i++)
 		cin >> s[i];
-	}
-	dfs(0, 0);
+	dfs(0, 0, 0);
 	cout << ans;
 }
