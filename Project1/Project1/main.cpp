3db24032731746d1029d7668c7330bd731b58286
bestway27@gmail.com <bestway27@gmail.com>
2020-06-19 11:18:57 +0900

    last

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,40 +1,23 @@
-#include <cstdio>
-#include <algorithm>
-struct Pos {
-	int x;
-	int y;
-}p1, p2;
-int m, n, k, a[100][100] = { 0 }, ans[6000], cnt = 0, area;
-void rec(Pos p1, Pos p2) {
-	for (int i = p1.x; i < p2.x; i++)
-		for (int j = p1.y; j < p2.y; j++)
-			a[i][j] = 1;
-}
-void dfs(int x, int y) {
-	if (x < 0 || x >= m || y < 0 || y >= n || a[x][y])
+#include <iostream>
+using namespace std;
+int r, c, ans = 0;
+char s[20][20];
+void dfs(int i, int j) {
+	if (i < 0 || i >= r || j < 0 || j >= c || !s[i][j])
 		return;
-	a[x][y] = 1;
-	area++;
-	dfs(x - 1, y);
-	dfs(x + 1, y);
-	dfs(x, y - 1);
-	dfs(x, y + 1);
+	ans++;
+	s[i][j] = 0;
+	dfs(i - 1, j);
+	dfs(i + 1, j);
+	dfs(i, j - 1);
+	dfs(i, j + 1);
 }
 int main() {
-	scanf("%d%d%d", &n, &m, &k);
-	while (k--) {
-		scanf("%d%d%d%d", &p1.x, &p1.y, &p2.x, &p2.y);
-		rec(p1, p2);
+	ios::sync_with_stdio(0);
+	scanf("%d%d", &r, &c);
+	for (int i = 0; i < r; i++) {
+		cin >> s[i];
 	}
-	for (int i = 0; i < m; i++)
-		for (int j = 0; j < n; j++)
-			if (!a[i][j]) {
-				area = 0;
-				dfs(i, j);
-				ans[cnt++] = area;
-			}
-	std::sort(ans, ans + cnt);
-	printf("%d\n", cnt);
-	for (int i = 0; i < cnt; i++)
-		printf("%d ", ans[i]);
+	dfs(0, 0);
+	cout << ans;
 }
