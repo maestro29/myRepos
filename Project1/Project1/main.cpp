b52950ebbf97b9ec146ab0f2ab2150928b7c605f
bestway27@gmail.com <bestway27@gmail.com>
2020-07-03 09:20:20 +0900

    last

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,24 +1,29 @@
-#include <iostream>
+#include <cstdio>
+#include <queue>
+#include <tuple>
+#define PUSH(x,y) if (a[x][y]=='0') q.push({x,y,w,v+1}), a[x][y] = '2';\
+				  else if (a[x][y] == '1' && !w) q.push({x,y,w+1,v+1}), a[x][y] = '2';
 using namespace std;
-int r, c, ans, v[26];
-char s[21][21];
-void dfs(int cnt, int i, int j) {
-	if (i < 0 || i >= r || j < 0 || j >= c || v[s[i][j] - 'A'])
-		return;
-	v[s[i][j] - 'A'] = 1;
-	if (++cnt > ans)
-		ans = cnt;
-	dfs(cnt, i - 1, j);
-	dfs(cnt, i + 1, j);
-	dfs(cnt, i, j - 1);
-	dfs(cnt, i, j + 1);
-	//v[s[i][j]] = 0;
+int n, m, x, y, w, v;
+queue<tuple<int, int, int, int>> q;
+char a[1000][1001];
+int bfs() {
+	PUSH(0, 0);
+	while (q.size()) {
+		tie(x,y,w,v) = q.front();
+		q.pop();
+		if (x == n - 1 && y == m - 1)
+			return v;
+		if (x) PUSH(x - 1, y);
+		if (y) PUSH(x, y - 1);
+		if (x < n - 1) PUSH(x + 1, y);
+		if (y < m - 1) PUSH(x, y + 1);
+	}
+	return -1;
 }
 int main() {
-	ios::sync_with_stdio(0);
-	scanf("%d%d", &r, &c);
-	for (int i = 0; i < r; i++)
-		cin >> s[i];
-	dfs(0, 0, 0);
-	cout << ans;
+	scanf("%d%d", &n, &m);
+	for (int i = 0; i < n; i++)
+		scanf("%s", a[i]);
+	printf("%d", bfs());
 }
