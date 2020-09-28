0f424d8b6ef04809096c79b65a79e7a78fe8e71f
bestway27@gmail.com <bestway27@gmail.com>
2020-09-28 12:19:41 +0900

    last

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,29 +1,62 @@
-#include <set>
+#include <string>
+#include <vector>
 using namespace std;
-int solution(int N, int number) {
-    set<int> s[9];
-    int nn = N;
-    if (N == number) return 1;
-    for (int i = 1; i <= 8; i++) {
-        s[i].insert(nn);
-        nn = nn * 10 + N;
-        for (int j = 1; j <= i / 2; j++) {
-            for (auto x : s[j])
-                for (auto y : s[i-j]) {
-                    s[i].insert(x + y);
-                    s[i].insert(x - y);
-                    s[i].insert(y - x);
-                    s[i].insert(x * y);
-                    s[i].insert(x / y);
-                    s[i].insert(y / x);
-                    if (s[i].find(number) != s[i].end())
-                        return i;
-                }
-            s[i].erase(0);
+enum type { ki, bo };
+int k[101][101], b[101][101], N;
+bool chk(type t, int x, int y) {
+    if (x < 0 || y < 0 || x > N || y > N)
+        return true;
+    switch (t) {
+    case ki:
+        if (k[x][y - 1] || b[x][y] || b[x - 1][y] || y == 0)
+            return true;
+        break;
+    case bo:
+        if (k[x][y - 1] || k[x + 1][y - 1] ||
+            (b[x - 1][y] && b[x + 1][y]))
+            return true;
+    }
+    return false;
+}
+vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
+    vector<vector<int>> answer;
+    N = n;
+    for (auto i : build_frame) {
+        int x = i[0], y = i[1];
+        switch (i[2]) {
+        case ki:
+            if (i[3] && chk(ki, x, y)) k[x][y]++;
+            if (!i[3]) {
+                k[x][y]--;
+                if ((b[x-1][y+1] && !chk(bo, x - 1, y + 1))
+                    || (b[x][y+1] && !chk(bo, x, y + 1))
+                    || (k[x][y+1] && !chk(ki, x, y + 1)))
+                    k[x][y]++;
+            }
+            break;
+        case bo:
+            if (i[3] && chk(bo, x, y)) b[x][y]++;
+            if (!i[3]) {
+                b[x][y]--;
+                if ((b[x-1][y] && !chk(bo, x - 1, y))
+                    || (b[x+1][y] && !chk(bo, x + 1, y))
+                    || (k[x][y] && !chk(ki, x, y))
+                    || (k[x+1][y] && !chk(ki, x + 1, y)))
+                    b[x][y]++;
+            }
         }
     }
-    return -1;
+    for (int i = 0; i < n + 1; i++)
+        for (int j = 0; j < n + 1; j++) {
+            if (k[i][j]) answer.push_back({ i,j,ki });
+            if (b[i][j]) answer.push_back({ i,j,bo });
+        }
+    return answer;
 }
 int main() {
-    printf("%d", solution(3, 3));
+    vector<vector<int>> answer;
+    answer = solution(5, { {0,0,0,1},{2,0,0,1},{4,0,0,1},{0,1,1,1},{1,1,1,1},{2,1,1,1},{3,1,1,1},{2,0,0,0},{1,1,1,0},{2,2,0,1} });
+    for (auto i : answer) {
+        printf("%d %d %d\n", i[0], i[1], i[2]);
+    }
 }
